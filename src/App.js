75f71236b0c17d65d9809923f02c495f71d301db
Iman Heidari <iman.heydari@gmail.com>
2021-03-17 20:46:36 -0700

    Add Tailwind css to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
 import Cart from "./components/Cart";
 import Menu from "./components/Menu";
 import Products from "./components/Products";
 
 function App() {
   return (
-    <div className="App">
+    <div className="p-4 w-96 mx-auto">
       <Menu />
       <Products />
       <Cart />
