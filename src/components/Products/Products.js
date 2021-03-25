e99b99462a7c0b3d8648d7d37de22425a8f93069
Iman Heidari <iman.heydari@gmail.com>
2021-03-24 19:57:54 -0700

    add mock data

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,32 +1,44 @@
 import Product from "./Product";
 import { Container } from "./styled";
 
+const mockData = [
+  {
+    id: 1,
+    name: "Pen",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg",
+    description: "Pen in different colors",
+    price: 5,
+  },
+  {
+    id: 2,
+    name: "Pencil",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg",
+    description: "Pencil in different colors",
+    price: "10",
+  },
+  {
+    id: 1,
+    name: "Cup",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg",
+    description: "Cup in different colors",
+    price: "4",
+  },
+];
+
 const Products = (props) => {
-  return (
-    <Container>
-      <Product
-        id="1"
-        name="Pen"
-        image="https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg"
-        description="Pen in different colors"
-        price="5"
-      />
-      <Product
-        id="1"
-        name="Pen"
-        image="https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg"
-        description="Pen in different colors"
-        price="5"
-      />
-      <Product
-        id="1"
-        name="Pen"
-        image="https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg"
-        description="Pen in different colors"
-        price="5"
-      />
-    </Container>
-  );
+  const productsComponent = mockData.map((product) => (
+    <Product
+      key={product.id}
+      name={product.name}
+      description={product.description}
+      image={product.image}
+      price={product.price}
+    />
+  ));
+  return <Container>{productsComponent}</Container>;
 };
 
 export default Products;
