55f2278e96d884b482276309608459a77cb89cfa
Iman Heidari <iman.heydari@gmail.com>
2021-03-26 23:00:07 -0700

    fix the test

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 
 const Menu = ({ cartCount }) => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/cart">
-          Cart(<span>{cartCount}</span>)
-        </Link>
-      </li>
-    </ul>
+    <nav>
+      <h1>Shopping</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            Cart(<span>{cartCount}</span>)
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
