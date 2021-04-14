a5e73bab6ddfcd028cdcf11bf577c19cfc660629
Ehsan_Heidari <ehsan.heydari@gmail.com>
2021-04-14 03:05:58 -0700

    Fix the empty cart page to show message

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,15 @@ import CartItem from "./CartItem";
 import { Container } from "./styled";
 
 const Cart = ({ cart, removeFromCart }) => {
+  if (cart.length === 0) {
+    var Empty = "Add Some Stuff!!";
+  }
   const cartItems = cart.map((item) => (
     <CartItem key={item.id} product={item} removeFromCart={removeFromCart} />
   ));
   return (
     <Container>
+      {Empty}
       {cartItems}
       <hr />
       Total Price: $
