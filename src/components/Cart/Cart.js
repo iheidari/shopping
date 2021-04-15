41a152308006ccd2b4069aa6421c34ebaffe4b92
Iman Heidari <iman.heydari@gmail.com>
2021-04-14 19:46:08 -0700

    add style to the cart and empty cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,31 @@
 import CartItem from "./CartItem";
-import { Container } from "./styled";
+import {
+  EmptyCart,
+  Container,
+  CartItems,
+  TotalPrice,
+  BottomRow,
+  ClearCart,
+} from "./styled";
 
-const Cart = ({ cart, removeFromCart }) => {
+const Cart = ({ cart, removeFromCart, removeAll }) => {
+  if (!cart.length) {
+    return <EmptyCart>No item in the cart</EmptyCart>;
+  }
+
   const cartItems = cart.map((item) => (
     <CartItem key={item.id} product={item} removeFromCart={removeFromCart} />
   ));
   return (
     <Container>
-      {cartItems}
-      <hr />
-      Total Price: $
-      {cart.reduce((total, item) => total + item.price * item.count, 0)}
+      <CartItems>{cartItems}</CartItems>
+      <BottomRow>
+        <TotalPrice>
+          Total Price: $
+          {cart.reduce((total, item) => total + item.price * item.count, 0)}
+        </TotalPrice>
+        <ClearCart onClick={removeAll}>Delete All</ClearCart>
+      </BottomRow>
     </Container>
   );
 };
