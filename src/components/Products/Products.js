251e97327af2da95d4b0d9baf7158db1f130b09a
Iman Heidari <iman.heydari@gmail.com>
2021-04-28 20:49:36 -0700

    retrieve the products from contentful

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import Product from "./Product";
 import { Container } from "./styled";
+import axios from "axios";
 
 const mockData = [
   {
@@ -10,26 +12,53 @@ const mockData = [
     description: "Pen in different colors",
     price: 5,
   },
-  {
-    id: 2,
-    name: "Pencil",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg",
-    description: "Pencil in different colors",
-    price: "10",
-  },
-  {
-    id: 3,
-    name: "Cup",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/81A%2Bbhs4jkL._AC_SL1500_.jpg",
-    description: "Cup in different colors",
-    price: "4",
-  },
 ];
 
 const Products = ({ addToCart }) => {
-  const productsComponent = mockData.map((product) => (
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // edge cases
+
+    // async:
+    // 1. callback -> old_async_function(callback_function)
+    // 2. promise -> promise_async_function().then(response=>{}).catch(error=>{}).finally(()=>{})
+    // 3. async -> async function async_function({  await async_call(); })
+    // 4. generator functions -> function*
+
+    axios
+      .get(
+        "https://cdn.contentful.com/spaces/9n2fjo2f8clh/environments/master/entries",
+        {
+          params: {
+            content_type: "product",
+            access_token: "S5-0jWr40c6N2oBYFe-cLQSnWExciLp4Ma3CaJ4nbyk",
+          },
+        }
+      )
+      .then((result) => {
+        if (result.status === 200) {
+          console.log(result.data);
+          const assets = result.data.includes.Asset;
+
+          setProducts(
+            result.data.items.map((item) => ({
+              id: item.sys.id,
+              name: item.fields.name,
+              price: item.fields.price,
+              description: item.fields.description,
+              image: assets.filter(
+                (asset) => asset.sys.id === item.fields.image.sys.id
+              )[0].fields.file.url,
+            }))
+          );
+        } else {
+          console.log("Error in products request");
+        }
+      });
+  }, []);
+
+  const productsComponent = products.map((product) => (
     <Product key={product.id} addToCart={addToCart} product={product} />
   ));
   return <Container>{productsComponent}</Container>;
